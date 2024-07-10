8bb07d719c300b6c107573919df6db1c05dec743
Yury1973 <abashkinyn@gmail.com>
2024-07-10 18:38:43 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+kolichestvo_vipolnennih_DZ = 12
+kolichestvo_zatrachennih_chasov = 1.5
+nazvanie_kursa = 'Python'
+vremya_na_odno_zadanie = 1.5/12
+print('Курс:', nazvanie_kursa, ', всего задач:', kolichestvo_vipolnennih_DZ, ', затрачено часов:',
+      kolichestvo_zatrachennih_chasov, ', среднее время выполнения', vremya_na_odno_zadanie, 'часа.')
